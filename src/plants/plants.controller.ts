e4f5b4a29e28e30bb54dc4de10cdaabf2583afcf
Erick Gonzalez <ericksinbox@gmail.com>
2024-05-16 22:03:20 +0200

    feat: set up role guard for post request

--- src/plants/plants.controller.ts
+++ src/plants/plants.controller.ts
@@ -10,17 +10,23 @@ import {
   HttpStatus,
   ParseIntPipe,
   ValidationPipe,
+  UseGuards,
 } from '@nestjs/common';
 import { CreatePlantDto, ListAllEntities, UpdatePlantDto } from './dto';
 import { PlantsService } from './plants.service';
 import { Plant } from './interfaces';
+import { RolesGuard } from 'src/roles.guard';
+import { Roles, Role } from 'src/roles.decorator';
 
 // Note:  Controllers should handle HTTP requests and delegate more complex tasks to providers. Providers are plain JavaScript classes that are declared as providers in a module.
 
 @Controller('plants')
+@UseGuards(RolesGuard)
 export class PlantsController {
   constructor(private plantService: PlantsService) {}
+
   @Post()
+  @Roles([Role.ADMIN])
   async create(@Body(new ValidationPipe()) createPlantDto: CreatePlantDto) {
     this.plantService.create(createPlantDto);
   }
